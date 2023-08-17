f3a232afced66c7dd12e89440922032223d1fc7e
ricky-kiva <ricky.kiva@gmail.com>
2023-08-17 22:52:00 +0700

    build(data-remote): add remote data source

---
+++ app/src/main/java/com/rickyslash/kidcineproval/core/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,42 @@
+package com.rickyslash.kidcineproval.core.data.source.remote
+
+import android.util.Log
+import com.rickyslash.kidcineproval.core.data.source.remote.network.ApiResponse
+import com.rickyslash.kidcineproval.core.data.source.remote.network.ApiService
+import com.rickyslash.kidcineproval.core.data.source.remote.response.MovieResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class RemoteDataSource(private val apiService: ApiService) {
+
+    suspend fun getAllMovies(): Flow<ApiResponse<List<MovieResponse>>> {
+        return flow {
+            try {
+                val response = apiService.getNowPlaying()
+                val result = response.results
+                if (result.isNotEmpty()) {
+                    emit(ApiResponse.Success(result))
+                } else {
+                    emit(ApiResponse.Empty)
+                }
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+                Log.e(TAG, e.toString())
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+    companion object {
+        val TAG: String = RemoteDataSource::class.java.simpleName
+
+        @Volatile
+        private var instance: RemoteDataSource? = null
+
+        fun getInstance(service: ApiService): RemoteDataSource =
+            instance ?: synchronized(this) {
+                instance ?: RemoteDataSource(service)
+            }
+    }
+}
