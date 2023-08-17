e7c383db8b184852bd11a7ed67535851a91830a6
ricky-kiva <ricky.kiva@gmail.com>
2023-08-17 22:52:45 +0700

    feat(core-utils): add convert id to genre func

--- app/src/main/java/com/rickyslash/kidcineproval/core/utils/utils.kt
+++ app/src/main/java/com/rickyslash/kidcineproval/core/utils/utils.kt
@@ -8,5 +8,30 @@ fun dateKebabToSentence(inputDate: String): String {
     val outputFormat = SimpleDateFormat("MMMM d, yyyy", Locale.US)
 
     val date = inputFormat.parse(inputDate)
-    return outputFormat.format(date)
+    return outputFormat.format(date!!)
+}
+
+fun getGenreNameFromId(genreId: Int): String {
+    return when (genreId) {
+        28 -> "Action"
+        12 -> "Adventure"
+        16 -> "Animation"
+        35 -> "Comedy"
+        80 -> "Crime"
+        99 -> "Documentary"
+        18 -> "Drama"
+        10751 -> "Family"
+        14 -> "Fantasy"
+        36 -> "History"
+        27 -> "Horror"
+        10402 -> "Music"
+        9648 -> "Mystery"
+        10749 -> "Romance"
+        878 -> "Science Fiction"
+        10770 -> "TV Movie"
+        53 -> "Thriller"
+        10752 -> "War"
+        37 -> "Western"
+        else -> "Unknown Genre"
+    }
 }
