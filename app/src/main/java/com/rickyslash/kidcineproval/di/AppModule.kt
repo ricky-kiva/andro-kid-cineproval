069ea9f6f2988d34c78909852b6d46a073fb1066
ricky-kiva <ricky.kiva@gmail.com>
2023-08-19 01:12:40 +0700

    feat(di): add app-scope DI modules

---
+++ app/src/main/java/com/rickyslash/kidcineproval/di/AppModule.kt
@@ -0,0 +1,15 @@
+package com.rickyslash.kidcineproval.di
+
+import com.rickyslash.kidcineproval.core.domain.usecase.MovieInteractor
+import com.rickyslash.kidcineproval.core.domain.usecase.MovieUseCase
+import com.rickyslash.kidcineproval.presentation.main.MainViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<MovieUseCase> { MovieInteractor(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { MainViewModel(get()) }
+}
