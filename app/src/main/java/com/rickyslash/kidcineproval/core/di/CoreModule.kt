dc2a79edaf5d71b70b243d63229799e64a28697b
ricky-kiva <ricky.kiva@gmail.com>
2023-08-19 01:12:16 +0700

    feat(core-di): add core DI modules

---
+++ app/src/main/java/com/rickyslash/kidcineproval/core/di/CoreModule.kt
@@ -0,0 +1,65 @@
+package com.rickyslash.kidcineproval.core.di
+
+import androidx.room.Room
+import com.rickyslash.kidcineproval.core.data.MovieRepository
+import com.rickyslash.kidcineproval.core.data.source.local.LocalDataSource
+import com.rickyslash.kidcineproval.core.data.source.local.room.MovieDatabase
+import com.rickyslash.kidcineproval.core.data.source.remote.RemoteDataSource
+import com.rickyslash.kidcineproval.core.data.source.remote.network.ApiService
+import com.rickyslash.kidcineproval.core.domain.repository.IMovieRepository
+import com.rickyslash.kidcineproval.core.utils.AppExecutors
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.viewmodel.BuildConfig
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<MovieDatabase>().movieDao() }
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            MovieDatabase::class.java,
+            "Movie.db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+}
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            })
+            .addInterceptor { chain ->
+                val req = chain.request()
+                val reqHeaders = req.newBuilder()
+                    .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwN2IxY2U1ZjQ1NmI0ODRmYWYyY2VkZjc2YTYyNTdiNiIsInN1YiI6IjY0ZGIwZDM1NzcxOWQ3MDEyMjYwYTIyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y7ox5FRn6KieG_aZt4rmPWdUvsKj83OEhBIRO5UbpqU")
+                    .build()
+                chain.proceed(reqHeaders)
+            }
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single { LocalDataSource(get()) }
+    single { RemoteDataSource(get()) }
+    factory { AppExecutors() }
+    single<IMovieRepository> { MovieRepository(get(), get(), get()) }
+}
