3a05145d575c10c65b73157666deeb4d3c52e800
ricky-kiva <ricky.kiva@gmail.com>
2023-08-19 16:46:29 +0700

    feat(presentation-activity): add Favorite activity

---
+++ app/src/main/java/com/rickyslash/kidcineproval/presentation/favorite/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.rickyslash.kidcineproval.presentation.favorite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rickyslash.kidcineproval.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
