c067e07bdf143cee9a726d36357f07e5d9d65cfe
ricky-kiva <ricky.kiva@gmail.com>
2023-08-24 01:03:47 +0700

    feat(di-module): add certificate pinning to networkModule

--- core/src/main/java/com/rickyslash/kidcineproval/core/di/CoreModule.kt
+++ core/src/main/java/com/rickyslash/kidcineproval/core/di/CoreModule.kt
@@ -8,6 +8,7 @@ import com.rickyslash.kidcineproval.core.data.source.remote.RemoteDataSource
 import com.rickyslash.kidcineproval.core.data.source.remote.network.ApiService
 import com.rickyslash.kidcineproval.core.domain.repository.IMovieRepository
 import com.rickyslash.kidcineproval.core.utils.AppExecutors
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -32,6 +33,12 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/5VLcahb6x4EvvFrCF2TePZulWqrLHS2jCg9Ywv6JHog=")
+            .add(hostname, "sha256/vxRon/El5KuI4vx5ey1DgmsYmRY0nDd5Cg4GfJ8S+bg=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
@@ -45,6 +52,7 @@ val networkModule = module {
             }
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
