5a78a4c0cd7e6900953800a31691ca8c25754828
ricky-kiva <ricky.kiva@gmail.com>
2023-08-24 00:06:54 +0700

    refactor(application): remove unused import and declaration

--- core/src/main/java/com/rickyslash/kidcineproval/core/data/source/local/room/MovieDatabase.kt
+++ core/src/main/java/com/rickyslash/kidcineproval/core/data/source/local/room/MovieDatabase.kt
@@ -1,8 +1,6 @@
 package com.rickyslash.kidcineproval.core.data.source.local.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.rickyslash.kidcineproval.core.data.source.local.entity.MovieEntity
 
--- core/src/main/java/com/rickyslash/kidcineproval/core/utils/utils.kt
+++ core/src/main/java/com/rickyslash/kidcineproval/core/utils/utils.kt
@@ -12,31 +12,6 @@ fun dateKebabToSentence(inputDate: String): String {
     return outputFormat.format(date!!)
 }
 
-fun getGenreNameFromId(genreId: Int): String {
-    return when (genreId) {
-        28 -> "Action"
-        12 -> "Adventure"
-        16 -> "Animation"
-        35 -> "Comedy"
-        80 -> "Crime"
-        99 -> "Documentary"
-        18 -> "Drama"
-        10751 -> "Family"
-        14 -> "Fantasy"
-        36 -> "History"
-        27 -> "Horror"
-        10402 -> "Music"
-        9648 -> "Mystery"
-        10749 -> "Romance"
-        878 -> "Science Fiction"
-        10770 -> "TV Movie"
-        53 -> "Thriller"
-        10752 -> "War"
-        37 -> "Western"
-        else -> "Unknown Genre"
-    }
-}
-
 fun approvalComment(voteAverage: Double): Int {
     return when {
         voteAverage >= 8.0 -> R.string.label_approved_80_up
